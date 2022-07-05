0617203591b28e92654f90f2b9525cccc36691d3
Péter Markó <peter.marko1089@gmail.com>
2022-07-05 10:50:32 +0200

    Modify: switch back to Data annotation.

--- cardgame/src/main/java/com/cardgameproject/cardgame/entity/Card.java
+++ cardgame/src/main/java/com/cardgameproject/cardgame/entity/Card.java
@@ -1,21 +1,18 @@
 package com.cardgameproject.cardgame.entity;
 
 import com.cardgameproject.cardgame.enums.rarityLevel;
-import lombok.*;
-import org.hibernate.Hibernate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name="discriminatorColumn")
 @Table(name="AbstractCard")
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public abstract class Card {
@@ -28,16 +25,4 @@ public abstract class Card {
     private String description;
     private rarityLevel rarity;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Card card = (Card) o;
-        return id != null && Objects.equals(id, card.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
