824580990ddeaad78b85392ba6e17709c656ec64
Péter Markó <peter.marko1089@gmail.com>
2022-07-05 10:52:28 +0200

    Add: Dependency injection and findByName method implementation.

--- cardgame/src/main/java/com/cardgameproject/cardgame/service/CreaturesCardService.java
+++ cardgame/src/main/java/com/cardgameproject/cardgame/service/CreaturesCardService.java
@@ -1,4 +1,20 @@
 package com.cardgameproject.cardgame.service;
+import com.cardgameproject.cardgame.entity.Creatures;
+import com.cardgameproject.cardgame.repositories.CreaturesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class CreaturesCardService {
+@Service
+public class CreaturesCardService implements CreaturesService {
+
+    private CreaturesRepository creatureRepository;
+    @Autowired
+    public CreaturesCardService(CreaturesRepository creatureRepository) {
+        this.creatureRepository = creatureRepository;
+    }
+
+    @Override
+    public Creatures findByName(String name) {
+        return creatureRepository.findByName(name);
+    }
 }
