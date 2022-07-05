f0ed64924f76d05dde56a51c7ce65e0311587ca3
Péter Markó <peter.marko1089@gmail.com>
2022-07-05 13:38:09 +0200

    Add: necessary annotations and dependency injection to the CardController class

--- cardgame/src/main/java/com/cardgameproject/cardgame/controller/CardController.java
+++ cardgame/src/main/java/com/cardgameproject/cardgame/controller/CardController.java
@@ -1,4 +1,20 @@
 package com.cardgameproject.cardgame.controller;
 
+import com.cardgameproject.cardgame.service.CreaturesCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins = {"http://localhost:3000"},
+        methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+        , allowedHeaders = "*")
+@RestController
 public class CardController {
+
+    private CreaturesCardService creatureService;
+
+    @Autowired
+    public CardController(CreaturesCardService creatureService) {
+        this.creatureService = creatureService;
+    }
 }
