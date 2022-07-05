e9fea12e480038df4320a9efe7d67ce37f9a2b6c
Péter Markó <peter.marko1089@gmail.com>
2022-07-05 13:08:08 +0200

    Add: findAllBybaseAttack, health and stateType method signatures creaturesService interface .

--- cardgame/src/main/java/com/cardgameproject/cardgame/service/CreaturesService.java
+++ cardgame/src/main/java/com/cardgameproject/cardgame/service/CreaturesService.java
@@ -1,10 +1,16 @@
 package com.cardgameproject.cardgame.service;
 
 import com.cardgameproject.cardgame.entity.Creatures;
+import com.cardgameproject.cardgame.enums.stateType;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface CreaturesService {
 
     Creatures findByName(String name);
+    List<Creatures> findAllBybaseAttack(int attack);
+    List<Creatures> findAllBybaseHealth(int health);
+    List<Creatures> findAllBystateType(stateType state);
 }
