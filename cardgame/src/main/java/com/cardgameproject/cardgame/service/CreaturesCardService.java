1937577187ae3b61d44bfd15685a21fd611a2e9d
Péter Markó <peter.marko1089@gmail.com>
2022-07-05 13:07:00 +0200

    Add: findAllBybaseAttack, health and stateType service methods.

--- cardgame/src/main/java/com/cardgameproject/cardgame/service/CreaturesCardService.java
+++ cardgame/src/main/java/com/cardgameproject/cardgame/service/CreaturesCardService.java
@@ -1,8 +1,11 @@
 package com.cardgameproject.cardgame.service;
 import com.cardgameproject.cardgame.entity.Creatures;
+import com.cardgameproject.cardgame.enums.stateType;
 import com.cardgameproject.cardgame.repositories.CreaturesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CreaturesCardService implements CreaturesService {
@@ -16,5 +19,20 @@ public class CreaturesCardService implements CreaturesService {
     @Override
     public Creatures findByName(String name) {
         return creatureRepository.findByName(name);
+    }
+
+    @Override
+    public List<Creatures> findAllBybaseAttack(int attack) {
+       return creatureRepository.findAllBybaseAttack(attack);
+    }
+
+    @Override
+    public List<Creatures> findAllBybaseHealth(int health) {
+        return creatureRepository.findAllBybaseHealth(health);
+    }
+
+    @Override
+    public List<Creatures> findAllBystateType(stateType state) {
+        return creatureRepository.findAllBystate(state);
     }
 }
