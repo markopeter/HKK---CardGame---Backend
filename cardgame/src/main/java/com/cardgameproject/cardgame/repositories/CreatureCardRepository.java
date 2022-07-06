22aa2ccca142e644b4c0fa47719b6d889cb6d622
Péter Markó <peter.marko1089@gmail.com>
2022-07-06 10:09:24 +0200

    Add: CreatureCardRepository

---
+++ cardgame/src/main/java/com/cardgameproject/cardgame/repositories/CreatureCardRepository.java
@@ -0,0 +1,8 @@
+package com.cardgameproject.cardgame.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CreatureCardRepository extends JpaRepository {
+}
