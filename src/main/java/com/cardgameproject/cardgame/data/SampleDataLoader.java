d81c324049918e7697e1ae7a386ef313f7c72a85
Péter Markó <peter.marko1089@gmail.com>
2022-07-10 13:44:45 +0200

    Add: data package and SampleDataLoader to it

---
+++ src/main/java/com/cardgameproject/cardgame/data/SampleDataLoader.java
@@ -0,0 +1,10 @@
+package com.cardgameproject.cardgame.data;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class SampleDataLoader implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
