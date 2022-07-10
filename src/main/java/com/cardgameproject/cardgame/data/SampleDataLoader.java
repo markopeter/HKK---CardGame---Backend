1ae5c5b61f7a25a4407e5b54e0ff24fdda8cec91
Péter Markó <peter.marko1089@gmail.com>
2022-07-10 15:28:28 +0200

    Add: 100 cards creation

--- src/main/java/com/cardgameproject/cardgame/data/SampleDataLoader.java
+++ src/main/java/com/cardgameproject/cardgame/data/SampleDataLoader.java
@@ -2,12 +2,14 @@ package com.cardgameproject.cardgame.data;
 
 import com.cardgameproject.cardgame.entity.CreatureCard;
 import com.cardgameproject.cardgame.repositories.CreatureCardRepository;
-import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.Random;
+
+import static com.cardgameproject.cardgame.enums.raceType.MONSTER;
+import static com.cardgameproject.cardgame.enums.rarityLevel.COMMON;
+import static com.cardgameproject.cardgame.enums.stateType.PASSIVE;
 
 @Component
 public class SampleDataLoader implements CommandLineRunner {
@@ -21,6 +23,23 @@ public class SampleDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
+        int amountOfCards = 100;
+        Random rand = new Random();
+        for(int i = 0; i < amountOfCards; i++) {
+            cardRepository.save(
+                    CreatureCard.builder()
+                            .baseAttack(rand.nextInt((10 - 1) + 1) + 1)
+                            .baseHealth(rand.nextInt((10 - 1) + 1) + 1)
+                            .canUseWeapon(true)
+                            .description("This the test card number : " + i)
+                            .manaCost(rand.nextInt((10 - 1) + 1) + 1)
+                            .name("Test " + i + " name ")
+                            .race(MONSTER)
+                            .rarity(COMMON)
+                            .imageUrl("No image")
+                            .state(PASSIVE)
+                            .build()
+            );
+        }
     }
 }
