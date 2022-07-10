deadcb2d98d89fe78916d6192850dfcdf085d22f
Péter Markó <peter.marko1089@gmail.com>
2022-07-10 17:04:00 +0200

    Bugfix: delete Faker bean

--- src/main/java/com/cardgameproject/cardgame/CardGameApplication.java
+++ src/main/java/com/cardgameproject/cardgame/CardGameApplication.java
@@ -9,8 +9,4 @@ public class CardGameApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CardGameApplication.class, args);
 	}
-	@Bean
-	Faker faker(){
-		return new Faker();
-	}
 }
