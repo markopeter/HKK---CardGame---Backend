c6661da2932fad727b05b8e1796eefd6b39ccd3d
Péter Markó <peter.marko1089@gmail.com>
2022-07-12 11:26:21 +0200

    Modify: Added deck creation and cards to different decks.

--- src/main/java/com/cardgameproject/cardgame/data/SampleDataLoader.java
+++ src/main/java/com/cardgameproject/cardgame/data/SampleDataLoader.java
@@ -1,10 +1,13 @@
 package com.cardgameproject.cardgame.data;
-
 import com.cardgameproject.cardgame.entity.CreatureCard;
+import com.cardgameproject.cardgame.entity.DeckEntity;
 import com.cardgameproject.cardgame.repositories.CreatureCardRepository;
+import com.cardgameproject.cardgame.repositories.DeckRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static com.cardgameproject.cardgame.enums.raceType.MONSTER;
@@ -16,33 +19,66 @@ public class SampleDataLoader implements CommandLineRunner {
 
 
     private final CreatureCardRepository cardRepository;
+    private final DeckRepository deckRepository;
 
-    public SampleDataLoader(CreatureCardRepository cardRepository) {
+    public SampleDataLoader(CreatureCardRepository cardRepository, DeckRepository deckRepository) {
         this.cardRepository = cardRepository;
+        this.deckRepository = deckRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        List<CreatureCard> sampleListOne = new ArrayList<>();
+        List<CreatureCard> sampleListTwo = new ArrayList<>();
+        List<CreatureCard> sampleListThree = new ArrayList<>();
         int amountOfCards = 100;
         String [] randomNames = {"SpookySkeleton", "BoogeyMan", "BigBadWolf", "ScaryGhost", "TerrificTroll"};
         Random rand = new Random();
         for(int i = 0; i < amountOfCards; i++) {
-            cardRepository.save(
-                    CreatureCard.builder()
-                            .baseAttack(rand.nextInt((10 - 1) + 1) + 1)
-                            .baseHealth(rand.nextInt((10 - 1) + 1) + 1)
-                            .canUseWeapon(true)
-                            .description("This the test card number : " + i)
-                            .manaCost(rand.nextInt((10 - 1) + 1) + 1)
-                            //TODO
-                            //Get rid of magic numbers, extract them out the variables
-                            .name(randomNames[rand.nextInt((4 - 1) + 1) + 1] + i)
-                            .race(MONSTER)
-                            .rarity(COMMON)
-                            .imageUrl("No image")
-                            .state(PASSIVE)
-                            .build()
-            );
+
+            CreatureCard testCard = CreatureCard.builder()
+                    .baseAttack(rand.nextInt((10 - 1) + 1) + 1)
+                    .baseHealth(rand.nextInt((10 - 1) + 1) + 1)
+                    .canUseWeapon(true)
+                    .description("This the test card number : " + i)
+                    .manaCost(rand.nextInt((10 - 1) + 1) + 1)
+                    //TODO
+                    //Get rid of magic numbers, extract them out the variables
+                    .name(randomNames[rand.nextInt((4 - 1) + 1) + 1] + i)
+                    .race(MONSTER)
+                    .rarity(COMMON)
+                    .imageUrl("No image")
+                    .state(PASSIVE)
+                    .build();
+
+            cardRepository.save(testCard);
+            if(i % 3 == 0){
+                sampleListOne.add(testCard);
+            }
+            if(i % 4 == 0){
+                sampleListTwo.add(testCard);
+            }
+            if(i % 5 == 0){
+                sampleListThree.add(testCard);
+            }
         }
+
+
+        DeckEntity sampleDeck1 = DeckEntity.builder()
+                .deckName("Sampleone")
+                .cards(sampleListOne)
+                .build();
+        DeckEntity sampleDeck2 = DeckEntity.builder()
+                .deckName("Sampletwo")
+                .cards(sampleListTwo)
+                .build();
+        DeckEntity sampleDeck3 = DeckEntity.builder()
+                .deckName("Samplethree")
+                .cards(sampleListThree)
+                .build();
+        deckRepository.save(sampleDeck1);
+        deckRepository.save(sampleDeck2);
+        deckRepository.save(sampleDeck3);
+
     }
 }
