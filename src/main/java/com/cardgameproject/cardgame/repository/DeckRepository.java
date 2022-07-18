c6a95bf51dded61302928eb55f72d3ee533a1db2
Péter Markó <peter.marko1089@gmail.com>
2022-07-18 21:01:05 +0200

    Modify: Repositories to Repository.

--- src/main/java/com/cardgameproject/cardgame/repository/DeckRepository.java
+++ src/main/java/com/cardgameproject/cardgame/repository/DeckRepository.java
@@ -1,4 +1,4 @@
-package com.cardgameproject.cardgame.repositories;
+package com.cardgameproject.cardgame.repository;
 
 import com.cardgameproject.cardgame.entity.DeckEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
