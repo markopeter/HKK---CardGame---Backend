cd4dd7b8bff990c7bc340dd86ab2f8d83f40a4bb
Péter Markó <peter.marko1089@gmail.com>
2022-08-02 14:24:34 +0200

    Delete: malfunctioning registration flow

--- src/main/java/com/cardgameproject/cardgame/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cardgameproject.cardgame.repository;
-
-import com.cardgameproject.cardgame.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
