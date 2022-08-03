20ac688e5d41b1ffd872042f756329a1419ad8ad
Péter Markó <peter.marko1089@gmail.com>
2022-08-03 21:37:20 +0200

    Add: Authority entity which implements the GrantedAuthority interface.

---
+++ src/main/java/com/cardgameproject/cardgame/entity/Authority.java
@@ -0,0 +1,22 @@
+package com.cardgameproject.cardgame.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Authority implements GrantedAuthority {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String authority;
+    @ManyToOne(optional = false)
+    private UserEntity user;
+}
