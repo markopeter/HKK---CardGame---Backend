edd6ce4e22c37a60330abc5526464a46313a0387
Péter Markó <peter.marko1089@gmail.com>
2022-08-04 10:44:03 +0200

    Modify: extend AuthController postmapping with requestbody and password hiding..

--- src/main/java/com/cardgameproject/cardgame/controller/AuthController.java
+++ src/main/java/com/cardgameproject/cardgame/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.cardgameproject.cardgame.controller;
 
 import com.cardgameproject.cardgame.dto.AuthCredentialRequest;
+import com.cardgameproject.cardgame.entity.UserEntity;
 import com.cardgameproject.cardgame.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +25,15 @@ public class AuthController {
     private JwtUtil jwtUtil;
 
     @PostMapping("login")
-    public ResponseEntity <?> login(AuthCredentialRequest request){
+    public ResponseEntity <?> login(@RequestBody AuthCredentialRequest request){
         try{
             Authentication authenticate = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             request.getUsername(), request.getPassword()));
-            User user = (User) authenticate.getPrincipal();
+            UserEntity user = (UserEntity) authenticate.getPrincipal();
+
+            //Temporary solution to hide the password
+            user.setPassword(null);
             return ResponseEntity.ok()
                     .header(HttpHeaders.AUTHORIZATION, jwtUtil.generateToken(user))
                     //TODO dto and mapper for userview
