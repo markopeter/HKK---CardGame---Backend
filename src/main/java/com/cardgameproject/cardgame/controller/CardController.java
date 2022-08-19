834e98b4c9decc467623ebdff9127ab25fc0356f
Péter Markó <peter.marko1089@gmail.com>
2022-08-19 15:17:59 +0200

    Add: RequestMapping to the CardController class .

--- src/main/java/com/cardgameproject/cardgame/controller/CardController.java
+++ src/main/java/com/cardgameproject/cardgame/controller/CardController.java
@@ -14,6 +14,7 @@ import java.util.List;
         methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
         , allowedHeaders = "*")
 @RestController
+@RequestMapping("/api")
 public class CardController {
 
     private CreatureCardService creatureService;
