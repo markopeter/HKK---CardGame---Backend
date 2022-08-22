082929c545225fc2fa1ed09c22681202cba013ce
Péter Markó <peter.marko1089@gmail.com>
2022-08-22 12:23:37 +0200

    Add: modelmapper bean.

--- src/main/java/com/cardgameproject/cardgame/CardGameApplication.java
+++ src/main/java/com/cardgameproject/cardgame/CardGameApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class CardGameApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardGameApplication.class, args);
 	}
