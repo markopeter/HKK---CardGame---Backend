dda7adf56be1fcde0464ca1902b9c117cccc85c9
Péter Markó <peter.marko1089@gmail.com>
2022-09-02 21:06:54 +0200

    Modify: stateType to monsterStateType.

--- src/main/java/com/cardgameproject/cardgame/converter/stateConverter.java
+++ src/main/java/com/cardgameproject/cardgame/converter/stateConverter.java
@@ -1,15 +1,15 @@
 package com.cardgameproject.cardgame.converter;
 
-import com.cardgameproject.cardgame.enums.stateType;
+import com.cardgameproject.cardgame.enums.monsterStateType;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.stream.Stream;
 
 @Converter(autoApply = true)
-public class stateConverter implements AttributeConverter<stateType, String> {
+public class stateConverter implements AttributeConverter<monsterStateType, String> {
     @Override
-    public String convertToDatabaseColumn(stateType stateType) {
+    public String convertToDatabaseColumn(monsterStateType stateType) {
         if (stateType == null) {
             return null;
         }
@@ -17,12 +17,12 @@ public class stateConverter implements AttributeConverter<stateType, String> {
     }
 
     @Override
-    public stateType convertToEntityAttribute(String code) {
+    public monsterStateType convertToEntityAttribute(String code) {
         if (code == null) {
             return null;
         }
 
-        return Stream.of(stateType.values())
+        return Stream.of(monsterStateType.values())
                 .filter(c -> c.getCode().equals(code))
                 .findFirst()
                 .orElseThrow(IllegalArgumentException::new);
